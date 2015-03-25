18f002a47a59db4b97a928ad74e83cf9af248c92
Matt Richmond <mrichmond@wearetmbr.com>
2015-03-24 23:10:32 -0600

    editing a client happens with one form now

--- html.js
+++ html.js
@@ -14,26 +14,21 @@ if (Meteor.isClient) {
       Clients.remove(this._id);
     },
     'click .edit': function(event){
-      var editBtn = $(event.target);
-      editBtn.parent().siblings('.name-holder').hide();
-      editBtn.parent().siblings('.name-edit').show();
+      var editBtn = $(event.target),
+          editForm = $('#edit-client');
+      editForm.find('input[name="clientId"]').val(this._id);
+      editForm.find('input[name="newname"]').val(this.name);
+      editForm.find('input[name="newmoney"]').val(this.money);
+      editForm.show();
+      editBtn.parent().siblings('.hide-when-edit').hide();
     },
-    'submit .edit-name': function(event){
-      var form = event.target,
-          newName = form.newname,
-          newMoney = form.newmoney,
-          $form = $(form);
-      Clients.update(this._id, {
-        name: newName.value,
-        money: newMoney.value
-      });
-      $form.parent().siblings('.name-holder').show();
-      $form.parent().hide();
-      return false;
-    }
-  })
+  });
 
   Template.addNew.events({
+    'click #show-add-new-client-form': function(event){
+      $('#add-new-client').show();
+      $('#show-add-new-client-form').hide();
+    },
     'submit': function(event) {
       var form = event.target,
           nameInput = form.name;
@@ -46,10 +41,35 @@ if (Meteor.isClient) {
 
       nameInput.value = "";
       moneyInput.value = "";
+      $('#add-new-client').hide();
+      $('#show-add-new-client-form').show();
       return false;
     }
-  })
+  });
+
+  Template.editClient.events({
+    'submit #edit-client': function(event){
+      var form = event.target,
+          newName = form.newname,
+          newMoney = form.newmoney,
+          clientId = form.clientId.value,
+          $form = $(form);
+      console.log(clientId);
+      Clients.update(clientId, {
+        name: newName.value,
+        money: newMoney.value
+      });
+
+      form.reset();
+      var clientRow = $('#client-' + clientId);
+      clientRow.find('.hide-when-edit').show();
+      $form.hide();
+      return false;
+    }
+  });
 }
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
